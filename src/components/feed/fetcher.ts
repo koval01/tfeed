e9a8c0c144d828093ee54c49ba77f49007974434
Koval Yaroslav <yaroslav@koval.page>
2024-11-09 16:45:10 +0100

    Update fetcher

--- src/components/feed/fetcher.ts
+++ src/components/feed/fetcher.ts
@@ -3,25 +3,21 @@ import useSWR from 'swr';
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
+const useInternalSWR = (url: string) => useSWR(
+    url, fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false
+});
+
 export const useMore = (channel: string, offset: number, refresh: boolean = false) => {
-    return useSWR(
-        `${process.env.NEXT_PUBLIC_API_HOST}/v1/more/${channel}/${!refresh ? "before" : "after"}/${offset}`,
-        fetcher, {
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false
-    }
-    );
+    const url = `${process.env.NEXT_PUBLIC_API_HOST}/v1/more/${channel}/${!refresh ? "before" : "after"}/${offset}`;
+    return useInternalSWR(url);
 }
 
 export const useBody = (channel: string, position?: number) => {
     const url = position === undefined
         ? `${process.env.NEXT_PUBLIC_API_HOST}/v1/body/${channel}`
         : `${process.env.NEXT_PUBLIC_API_HOST}/v1/body/${channel}?position=${position}`;
-
-    return useSWR(url, fetcher, {
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false
-    });
+    return useInternalSWR(url);
 }
