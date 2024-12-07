bd5d8b21c935bdbdd3f935770dccbaf2cd577956
Koval Yaroslav <yaroslav@koval.page>
2024-12-07 17:59:00 +0100

    Update Gif component

--- src/components/feed/post/GifPost.tsx
+++ src/components/feed/post/GifPost.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { Post } from "@/types";
 
+import { Spacing } from "@vkontakte/vkui";
+
 export const GifPost = React.memo(({ post }: { post: Post }) => {
     // Only render if media is GIF
     if (!post.content.media?.[0] || post.content.media[0].type !== "gif") {
@@ -12,19 +14,22 @@ export const GifPost = React.memo(({ post }: { post: Post }) => {
     const thumb = media.thumb;
 
     return (
-        <div className="block">
-            <video
-                className="max-h-svh h-auto w-full rounded-lg"
-                src={url}
-                poster={thumb}
-                controls={false}
-                autoPlay
-                muted
-                loop
-                playsInline
-                disablePictureInPicture
-            />
-        </div>
+        <>
+            {post.content.text && <Spacing size={12} />}
+            <div className="block">
+                <video
+                    className="max-h-svh h-auto w-full rounded-lg"
+                    src={url}
+                    poster={thumb}
+                    controls={false}
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    disablePictureInPicture
+                />
+            </div>
+        </>
     );
 });
 
