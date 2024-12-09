74a6fdee8c45235c61d8ea348bf4e7f6d951f2a9
Koval Yaroslav <yaroslav@koval.page>
2024-12-09 12:06:42 +0100

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const production = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
     compiler: {
-        removeConsole: true,
+        removeConsole: production,
         emotion: true,
         styledComponents: {
             pure: true
